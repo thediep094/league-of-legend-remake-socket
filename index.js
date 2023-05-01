8dc23262575c0623cdce5703e802f074dd96b4a8
thediep094 <thediep094@gmail.com>
2023-05-01 21:11:14 +0700

    dấdas

--- index.js
+++ index.js
@@ -15,24 +15,24 @@ const io = require("socket.io")(http, {
     origin: "*",
   },
 });
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
-
-  socket.on("send_message", (data) => {
-    console.log(data);
-    socket.broadcast.emit("receive_alert", data);
-    socket.to(data.room).emit("receive_message", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
-});
 http.listen(port, () => {
   console.log(`Server listening on ${port}`);
+  io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on("join_room", (data) => {
+      socket.join(data);
+      console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
+
+    socket.on("send_message", (data) => {
+      console.log(data);
+      socket.broadcast.emit("receive_alert", data);
+      socket.to(data.room).emit("receive_message", data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User Disconnected", socket.id);
+    });
+  });
 });
